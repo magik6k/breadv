e59cbeaf9dc8e3dedc86da12304d8131456d8c31
Łukasz Magiera <magik6k@gmail.com>
2021-10-10 14:14:55 +0200

    add use_rs decoding to op lut

--- ucode/decode_use_op.go
+++ ucode/decode_use_op.go
@@ -7,14 +7,22 @@ func main() {
 	var out [size]byte
 
 	var (
-		op_I byte = 0b00001
-		op_S byte = 0b00010
-		op_B byte = 0b00100
-		op_U byte = 0b01000
-		op_J byte = 0b10000
+		// out
+		op_I byte = 0b00001_000
+		op_S byte = 0b00010_000
+		op_B byte = 0b00100_000
+		op_U byte = 0b01000_000
+		op_J byte = 0b10000_000
+
+		rs1_disable byte = 0b00000_001
+		rs2_disable byte = 0b00000_010
+
+		// in
+		rs1_zero = 0b01_000_00000
+		rs2_zero = 0b10_000_00000
 	)
 
-	inmap := map[int]byte{
+	opmap := map[int]byte{
 		0b11100: op_I, // System (csr/e)
 		0b11001: op_I, // JALR
 		0b00000: op_I, // Load
@@ -30,8 +38,42 @@ func main() {
 		0b11011: op_J, // JAL
 	}
 
+	rs2map := map[int]byte{
+		0b11000: rs2_disable,
+		0b01000: rs2_disable,
+		0b01100: rs2_disable,
+	}
+
+	rs1amap := map[int]byte{
+		0b01101: rs1_disable,
+		0b00101: rs1_disable,
+		0x11011: rs1_disable,
+	}
+
+	rs1fmap := map[int]byte{
+		0b000_11100: rs1_disable,
+		0b101_11100: rs1_disable,
+		0b110_11100: rs1_disable,
+		0b111_11100: rs1_disable,
+	}
+
 	for i := range out {
-		out[i] = inmap[i&0x1f] << 3 // shift 3 for more convenient pinout
+		// write ops first
+		out[i] = opmap[i&0x1f]
+
+		// rs2
+		out[i] |= rs2map[i&0x1f]^rs2_disable
+		if i&rs2_zero > 0 {
+			out[i] |= rs2_disable
+		}
+
+		// rs1
+		out[i] |= rs1amap[i&0x1f]
+		out[i] |= rs1fmap[i&0x7f]
+		if i&rs1_zero > 0 {
+			out[i] |= rs1_disable
+		}
+
 	}
 
 	if err := ioutil.WriteFile("decode_use_op.bin", out[:], 0664); err != nil {
